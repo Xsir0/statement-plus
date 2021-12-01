01145c2754f2038f75b008d905cbd63964cfbeee
Xsir0 <48853107+Xsir0@users.noreply.github.com>
2021-12-01 21:01:09 +0800

    add controller

---
+++ src/main/java/com/example/statementplus/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.example.statementplus.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName TestController
+ * @Description
+ * @Author xsir
+ * @Date 2021/12/1 20:55
+ * @Version V1.0
+ */
+@RestController
+@RequestMapping("test")
+public class TestController {
+
+    @GetMapping("/hello")
+    public String test(){
+        return "Hi!";
+    }
+
+}
